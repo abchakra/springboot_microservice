a253f0bdfcc7b773603ae37026bf15a156e1d7eb
chakraborty <chakraborty@silexica.com>
2020-05-06 20:17:42 +0200

    Add rest test for gallery

---
+++ app-gallery-service/src/test/java/com/example/gallery/GalleryRestTest.java
@@ -0,0 +1,33 @@
+package com.example.gallery;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GalleryRestTest {
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testGalleryHome() throws Exception {
+		MvcResult result = this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andReturn();
+		String content = result.getResponse().getContentAsString();
+		assertEquals("Hello from Gallery Service running at port: null", content);
+	}
+	
+
+
+}
