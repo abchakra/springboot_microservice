21da30df8b9ea7f8574e701c68ec8ec8d75909e9
chakraborty <chakraborty@silexica.com>
2020-05-07 16:39:09 +0200

    use yml for properties file and create maven  module to build all projects

--- app-image-service/src/test/java/com/example/image/AppImageServiceApplicationTests.java
+++ app-image-service/src/test/java/com/example/image/AppImageServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.common;
+package com.example.image;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AppCommonServiceApplicationTests {
+class AppImageServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
